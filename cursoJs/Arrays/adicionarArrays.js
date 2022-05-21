c75e7bafaf7792880aa7fe8a4cdf7b99924619e7
renanribs <renanrbrs@gmail.com>
2022-05-21 12:10:59 -0300

    Aula 01 arrays

---
+++ cursoJs/Arrays/adicionarArrays.js
@@ -0,0 +1,6 @@
+const notas = [10, 6, 8];
+notas.push(7);
+
+console.log(`Notas: ${notas}`);
+
+let media = notas[0] + notas[1] + notas[2] + notas[3];
---
+++ cursoJs/Arrays/media.js
@@ -0,0 +1,18 @@
+/*
+let nota1 = 10;
+let nota2 = 6.5;
+let nota3 = 8;
+let nota4 = 7.5;
+*/
+
+function media(nota1, nota2, nota3, nota4) {
+  return (nota1 + nota2 + nota3 + nota4) / 4;
+}
+
+console.log(media(10, 6.5, 8, 7.5));
+
+const notas = [10, 6.5, 8, 7.5];
+
+let media2 = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media2);
