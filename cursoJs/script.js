3c0cf449656776af5a0be71b6ffb08b0207a4add
renanribs <renanrbrs@gmail.com>
2022-05-20 22:37:48 -0300

    Aula 02

---
+++ cursoJs/script.js
@@ -0,0 +1,19 @@
+// var
+
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura * comprimento;
+
+// ************************************
+const forma = "quadrado";
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === "quadrad") {
+  area = altura * comprimento;
+} else {
+  area = (altura * comprimento) / 2;
+}
+console.log(area);
