b0da7d8fe1c78fe17661c4733bb8b8af38aa17e6
renanribs <renanrbrs@gmail.com>
2022-06-03 18:48:36 -0300

    Update

--- cursoJs/beecrowd.js
+++ cursoJs/beecrowd.js
@@ -1,10 +1,11 @@
 let input = require("fs").readFileSync("stdin/file.txt", "utf8");
-let valor = input.split("\n");
+var valor = input.split("\n");
 
 let total = 0;
-for (let index = 0; index < valor.length; index++) {
-  if (valor[index] > 0) {
+for (i = 0; i <= 5; i++) {
+  if (Number(valor[i]) % 2 === 0) {
     total++;
   }
 }
-console.log(`${total} valores positivos`);
+
+console.log(`${total} valores pares`);
