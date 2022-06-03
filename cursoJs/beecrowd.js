8306fac8ef49a783fd2946abc84aac0a8a03de1f
renanribs <renanrbrs@gmail.com>
2022-06-03 02:17:11 -0300

    update

--- cursoJs/beecrowd.js
+++ cursoJs/beecrowd.js
@@ -1,13 +1,10 @@
 let input = require("fs").readFileSync("stdin/file.txt", "utf8");
-var lines = input.split(" ");
+let valor = input.split("\n");
 
-var N = parseInt(lines.shift());
-var N2 = N % 365;
-var N3 = N2 % 30;
-var anos = parseInt(N / 365);
-var meses = parseInt(N2 / 30);
-var dias = parseInt(N3);
-
-console.log(anos + " ano(s)");
-console.log(meses + " mes(es)");
-console.log(dias + " dia(s)");
+let total = 0;
+for (let index = 0; index < valor.length; index++) {
+  if (valor[index] > 0) {
+    total++;
+  }
+}
+console.log(`${total} valores positivos`);
