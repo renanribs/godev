70a360377c209ebc3d0993d785144c4086c70ab5
renanribs <renanrbrs@gmail.com>
2022-06-05 10:26:00 -0300

    Update

--- app-02/modules/fs.js
+++ app-02/modules/fs.js
@@ -4,7 +4,7 @@ const path = require("path");
 //criar uma pasta
 fs.mkdir(path.join(__dirname, "/test"), (error) => {
   if (error) {
-    return console.log("Erro: ", erro);
+    return console.log("Error: ", erro);
   }
   console.log("Pasta criada com Sucesso!");
 });
