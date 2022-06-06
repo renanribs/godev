084a834cc8f4e75c4097e326f69c975ce31a4b74
renanribs <renanrbrs@gmail.com>
2022-06-06 18:24:27 -0300

    Update

--- app-02/modules/fs.js
+++ app-02/modules/fs.js
@@ -1,10 +1,37 @@
 const fs = require("fs");
 const path = require("path");
 
-//criar uma pasta
+// //criar uma pasta
 fs.mkdir(path.join(__dirname, "/test"), (error) => {
   if (error) {
-    return console.log("Error: ", erro);
+    return console.log("Error: ", error);
   }
+
   console.log("Pasta criada com Sucesso!");
 });
+
+// Criar arquivo
+
+// fs.writeFile(
+//   path.join(__dirname, "/test", "test.txt"),
+//   "Hello node!",
+//   (error) => {
+//     if (error) {
+//       return console.log("Erro: ", error);
+//     }
+//     console.log("Arquivo criado com sucesso!");
+//   }
+// );
+
+//Adicionar a um arquivo
+
+fs.appendFile(
+  path.join(__dirname, "test", "test.txt"),
+  "Hello World",
+  (error) => {
+    if (error) {
+      return console.log("Erro: ", error);
+    }
+    console.log("Arquivo modificado com sucesso!");
+  }
+);
