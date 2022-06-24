b2896ebdd9197eded8c0bc9d2a00b71f36086bc7
renanribs <renanrbrs@gmail.com>
2022-06-23 22:13:04 -0300

    Update

--- cursoJs/Bibliotecas/index.js
+++ cursoJs/Bibliotecas/index.js
@@ -1,17 +1,28 @@
 import chalk from "chalk";
 import fs from "fs";
 
-function trataErro(erro) {
-  throw new Error(chalk.red(erro.code, "não foi possivel encontrar o arquivo"));
+function extraiLinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+  while ((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] });
+  }
+  return arrayResultados;
 }
 
-function pegaArquivo(caminhoDoArquivo) {
-  const encoding = "utf-8";
-  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-    if (erro) {
-      trataErro(erro);
-    }
-    console.log(chalk.green(texto));
-  });
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, "não há arquivo no caminho"));
 }
+
+async function pegaArquivo(caminhoDoArquivo) {
+  const encoding = "utf-8";
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(extraiLinks(texto));
+  } catch (erro) {
+    trataErro(erro);
+  }
+}
+
 pegaArquivo("./arquivos/texto.md");
