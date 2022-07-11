e7801599484c7f90bc712cdcec07a964111fddca
renanribs <renanrbrs@gmail.com>
2022-07-11 20:01:17 -0300

    Update

--- curso-react/src/App.jsx
+++ curso-react/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import Tasks from "./components/Tasks";
 import "./App.css";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   // let message = "Hello world!";
@@ -21,6 +22,7 @@ const App = () => {
   return (
     <>
       <div className="container">
+        <AddTask />
         <Tasks tasks={tasks} />
       </div>
       ;
